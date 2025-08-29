ca667719ee14b0b257e83a3f63169a02402cd433
ornateowl <139507059+ornateowl@users.noreply.github.com>
2025-08-29 20:47:08 +0000

    test edit

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 
 print("Welcome to the food ordering chatbot")
 name = input("what's your name?")
-age = input("how old are you?")
-print("good to know")
+age = input("Hello, " + name + " how old are you?")
+print("Good to know")
+
+print("Please choose from the following options")
+print("1. Placeholder 1")
+print("2. Placeholder 2")
+print("3. Placeholder 3")
+print("4. Exit")
+number = input("Enter the number of your choice")
+if number == 4:
+    print("Goodbye, "+ name)
